0e3a11afdd3e485153857d67fc98cfae8a6c2d78
Matthew <Matthew@10.0.0.134>
2017-09-22 15:48:32 -0400

    initial commit

---
+++ MiniProject7/src/Project7.java
@@ -0,0 +1,17 @@
+// Project 7
+// Matthew Gibney
+// Prints out a series of sentences
+
+public class Project7 {
+
+	public static void main(String[] args) {
+		System.out.println("A \"quoted\" String is");
+		System.out.println("\t\'much\' better if you learn");
+		System.out.println("the rules of \"escape sequences.\"");
+		System.out.println("\tAlso, \"\" represents an empty String.");
+		System.out.println("Dont't forget: use \\\" instead of \" !");
+		System.out.println("\t\'\' is not the same as \"");
+
+	}
+
+}
