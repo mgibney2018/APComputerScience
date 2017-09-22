9f2d336e47eea5ee59e6569012e2332cd0b1e35b
mgibney2018 <mgibney2018@cchsraider.net>
2017-09-22 10:07:52 -0400

    initial commit

---
+++ BlankLines/src/BlankLines.java
@@ -0,0 +1,13 @@
+
+public class BlankLines {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		blankLines();
+		System.out.println("World");
+	}
+	public static void blankLines() {
+		System.out.println("\n\n");
+	}
+}
